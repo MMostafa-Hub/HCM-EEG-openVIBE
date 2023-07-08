5c271c366ed3ac01ddaa9a695a6a25e3ec7ecdd9
Mohamed Mostafa <mmostafa.ibra21@gmail.com>
2023-07-08 16:43:29 +0300

    Final form of the EEGClient app before sockets

--- client/eeg_client.py
+++ client/eeg_client.py
@@ -1,7 +1,5 @@
 """ Refer back to http://openvibe.inria.fr/tutorial-using-python-with-openvibe/ for how to use openvibe framework """
 
-import numpy as np
-
 
 class EEGClientBox(OVBox):
     def __init__(self):
@@ -22,11 +20,8 @@ class EEGClientBox(OVBox):
                 self.signal_header = chunk
 
             elif isinstance(chunk, OVSignalBuffer):
-                # The data is sent as a 1D array, so we need to reshape it to the original shape
-                # The original shape is stored in the signal header
-                eeg_array = np.array(chunk).reshape(
-                    tuple(self.signal_header.dimensionSizes)
-                )
+                # TODO: send chunk over the socket
+                pass
 
             elif isinstance(chunk, OVSignalEnd):
                 print("End of signal reached!")
