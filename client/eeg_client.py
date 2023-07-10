cf47f67cddc31627011a4ce85d4cb9ad46eb70b8
Mohamed Mostafa <mmostafa.ibra21@gmail.com>
2023-07-10 15:38:53 +0300

    Refactor EEGClientBox class to use TCP socket for sending EEG signal data

--- client/eeg_client.py
+++ client/eeg_client.py
@@ -1,26 +1,26 @@
 class EEGClientBox(OVBox):
     def __init__(self):
+        OVBox.__init__(self)
         import socket
 
-        OVBox.__init__(self)
         self.tcp_socket: socket.socket
 
     def initialize(self):
         import socket
 
         self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_address = ("localhost", 12345)
+        server_address = ("192.168.220.97", 12345)
         self.tcp_socket.connect(server_address)
         print("Initializing EEG Client Box")
 
     def process(self):
         import pickle as pkl
+        import time
 
         eeg_signal_stream = self.input[0]
         while eeg_signal_stream:
             chunk = eeg_signal_stream.pop()
-            # print(type(chunk))
-            print(pkl.dumps(chunk))
+            time.sleep(1 / 64)  # 64 Hz
             self.tcp_socket.send(pkl.dumps(chunk))
 
     def uninitialize(self):
