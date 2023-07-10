101c273d02fdb0e79568dff75cb27fc4349e5779
Mohamed Mostafa <mmostafa.ibra21@gmail.com>
2023-07-10 15:33:59 +0300

    Refactor EEGServerBox class to use State enum for tracking connection state

--- server/eeg_server.py
+++ server/eeg_server.py
@@ -5,32 +5,44 @@ class State(Enum):
     INIT = 0
     SENDING = 1
     RECEIVING = 2
+
+
 class EEGServerBox(OVBox):
     def __init__(self):
         OVBox.__init__(self)
         import socket
 
         self.connection: socket.socket
+        self.tcp_socket: socket.socket
+
+        self.state: State = State.INIT
 
     def initialize(self):
-        import socket 
-
         print("Initializing EEG Server Box")
-        # Create a TCP/IP socket
-        tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_address = ("localhost", 12345)
-        tcp_socket.bind(server_address)
-        tcp_socket.listen(1)
+        import socket
 
-        self.connection, client_address = tcp_socket.accept()
-        print("Connection from: ", client_address)
+        # Create a TCP/IP socket
+        self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Bind the socket to the port
+        server_address = ("localhost", 12345)
+        self.tcp_socket.bind(server_address)
 
     def process(self):
-        import pickle as pkl
+        # Wait for a connection
+        if self.state == State.INIT:
+            self.tcp_socket.listen(1)
+            self.connection, client_address = self.tcp_socket.accept()
+            print("Connection from", client_address)
 
-        data = self.connection.recv(4096)
-        chunk = pkl.loads(data)
-        print(chunk)
+            self.state = State.RECEIVING
+        # Receive the data in small chunks and output it
+        elif self.state == State.RECEIVING:
+            import pickle as pkl
+
+            data = self.connection.recv(4096)
+            chunk = pkl.loads(data)
+            self.output[0].append(chunk)
 
     def uninitialize(self):
         print("Uninitialized EEG Server Box")
