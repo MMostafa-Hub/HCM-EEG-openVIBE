c182f8cc38473191383c8d3073231aeba9c78571
Mohamed Mostafa <mmostafa.ibra21@gmail.com>
2023-07-10 10:40:58 +0300

    Implement EEG client box

--- client/eeg_client.py
+++ client/eeg_client.py
@@ -1,30 +1,27 @@
-""" Refer back to http://openvibe.inria.fr/tutorial-using-python-with-openvibe/ for how to use openvibe framework """
-
-
 class EEGClientBox(OVBox):
     def __init__(self):
+        import socket
+
         OVBox.__init__(self)
-        self.signal_header = None
+        self.tcp_socket: socket.socket
 
     def initialize(self):
+        import socket
+
+        self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_address = ("localhost", 12345)
+        self.tcp_socket.connect(server_address)
         print("Initializing EEG Client Box")
 
     def process(self):
+        import pickle as pkl
+
         eeg_signal_stream = self.input[0]
         while eeg_signal_stream:
             chunk = eeg_signal_stream.pop()
-
-            # signal header is the first chunk of data sent
-            # contains startTime, endTime, dimensionSizes, dimensionLabels, samplingRate
-            if isinstance(chunk, OVSignalHeader):
-                self.signal_header = chunk
-
-            elif isinstance(chunk, OVSignalBuffer):
-                # TODO: send chunk over the socket
-                pass
-
-            elif isinstance(chunk, OVSignalEnd):
-                print("End of signal reached!")
+            # print(type(chunk))
+            print(pkl.dumps(chunk))
+            self.tcp_socket.send(pkl.dumps(chunk))
 
     def uninitialize(self):
         print("Uninitialized EEG Client Box")
