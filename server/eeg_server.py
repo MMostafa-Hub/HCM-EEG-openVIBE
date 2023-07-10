a30c3a70afb180d357f36a34ab6195698cd980f2
Mohamed Mostafa <mmostafa.ibra21@gmail.com>
2023-07-10 10:40:38 +0300

    Implement EEG Server box

--- server/eeg_server.py
+++ server/eeg_server.py
@@ -1,13 +1,29 @@
 class EEGServerBox(OVBox):
     def __init__(self):
         OVBox.__init__(self)
-        self.signal_header = None
+        import socket
+
+        self.connection: socket.socket
 
     def initialize(self):
+        import socket 
+
         print("Initializing EEG Server Box")
+        # Create a TCP/IP socket
+        tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_address = ("localhost", 12345)
+        tcp_socket.bind(server_address)
+        tcp_socket.listen(1)
+
+        self.connection, client_address = tcp_socket.accept()
+        print("Connection from: ", client_address)
 
     def process(self):
-        pass
+        import pickle as pkl
+
+        data = self.connection.recv(4096)
+        chunk = pkl.loads(data)
+        print(chunk)
 
     def uninitialize(self):
         print("Uninitialized EEG Server Box")
