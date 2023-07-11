b36d94231cf6d91e6c2f0afef77140a05b6a71f5
Mohamed Mostafa <mmostafa.ibra21@gmail.com>
2023-07-11 16:20:16 +0300

    Use UDP instead of TCP sockets

--- publisher/eeg_publisher.py
+++ publisher/eeg_publisher.py
@@ -3,30 +3,21 @@ class EEGPublisherBox(OVBox):
         OVBox.__init__(self)
         import socket
 
-        self.tcp_socket: socket.socket
+        self.udp_socket: socket.socket
+        self.subscriber_address = ("localhost", 12345)
 
     def initialize(self):
         print("Initializing EEG publisher Box")
         import socket
 
-        # Create a TCP/IP socket
-        self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        # Connect the socket to the port where the server is listening
-        subscriber_address = ("localhost", 12345)
-        self.tcp_socket.connect(subscriber_address)
+        # Create a UDP socket
+        self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     def process(self):
         import pickle as pkl
-        import time
 
-        # Take the EEG signal from the input buffer
-        eeg_signal_stream = self.input[0]
-        while eeg_signal_stream:
-            chunk = eeg_signal_stream.pop()
-            # Send the data in small chunks
-            time.sleep(1 / 64)  # 64 Hz
-            self.tcp_socket.send(pkl.dumps(chunk))
+        command = input("Enter command: ")
+        self.udp_socket.sendto(pkl.dumps(command), self.subscriber_address)
 
     def uninitialize(self):
         print("Uninitialized EEG publisher Box")
